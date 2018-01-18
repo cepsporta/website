2d9ed286a7b514479d63f1022220b104945911ad
ADev <amalchandrandev@gmail.com>
2018-01-18 09:00:36 +0530

    beautified timeline

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,51 +1,57 @@
 import React from "react";
 
 let data = [{
-    time: "16-January-2018",
+    time: "17 January 2018",
     posts: [
         {
-            head: "SPORTA LAUNCHED",
-            content: "The Sports Event is officially launched in by Principal of College Of Engineering Poonjar."
-        },
-        {
-            head: "CRICKET",
-            content: "Two matches were conducted. The first match was between Karnan and Napoleon. Karnan qualified to next round.The second match was between Bhagat Singh and Napoleon. Bhagat Singh was qualified."
-        },
-        {
-            head: "BADMINTON BOYS(Singles)",
-            content: "There were 2 matches. The first match was between Bhagat Singh and Napoleon. The match was won by Napoleon. The second match was between Bhagat Singh and Karnan. Only one set of game was conducted."
-        },
-        {
-            head: "BADMINTON GIRLS(Singles)",
-            content: "There were 2 matches.The first match was between Bhagat Singh and Napoleon. The match was won by Napoleon. The second match was between Bhagat Singh and Karnan. Only one set of game was conducted."
-        }
-    ]
-},{
-    time: "17-January-2018",
-    posts: [
-        {
-            head: "CRICKET",
+            head: "Cricket",
             content: "Cricket final was conducted. The match was between Bhagat Singh and Karnan. Bhagat Singh won the match."
         },
         {
-            head: "BADMINTON BOYS(Singles)",
+            head: "Badminton Boys",
+            headSub: "Singles",
             content: "Final match was conducted between Bhagat Singh and Napoleon. The match was won by Bhagat Singh."
         },
         {
-            head: "BADMINTON GIRLS(Singles)",
+            head: "Badminton Girls",
+            headSub: "Singles",
             content: "There were 2 matches. The first match was between Bhagat Singh and Karnan.The match was won by Bhagat Singh. The second match was final and the match was between Napoleon and Bhagat Singh. The finals was won by Napoleon."
         },
         {
-            head: "BADMINTON BOYS(Doubles)",
+            head: "Badminton Boys",
+            headSub: "Doubles",
             content: ""
         },
         {
-            head: "BADMINTON GIRLS(Doubles)",
+            head: "Badminton Girls",
+            headSub: "Doubles",
             content: ""
         },
         {
-            head:"VOLLEY BALL",
+            head: "Volley Ball",
             content: ""
+        }
+    ]
+}, {
+    time: "16 January 2018",
+    posts: [
+        {
+            head: "Sporta Launched",
+            content: "The Sports Event is officially launched in by Principal of College Of Engineering Poonjar."
+        },
+        {
+            head: "Cricket",
+            content: "Two matches were conducted. The first match was between Karnan and Napoleon. Karnan qualified to next round.The second match was between Bhagat Singh and Napoleon. Bhagat Singh was qualified."
+        },
+        {
+            head: "Badminton Boys",
+            headSub: "Singles",
+            content: "There were 2 matches. The first match was between Bhagat Singh and Napoleon. The match was won by Napoleon. The second match was between Bhagat Singh and Karnan. Only one set of game was conducted."
+        },
+        {
+            head: "Badminton Girls",
+            headSub: "Singles",
+            content: "There were 2 matches.The first match was between Bhagat Singh and Napoleon. The match was won by Napoleon. The second match was between Bhagat Singh and Karnan. Only one set of game was conducted."
         }
     ]
 }
@@ -58,16 +64,10 @@ export default () => {
                 <div key={i} className="timeline-item" date-is={singleLine.time}>
                     {singleLine.hasOwnProperty("posts") ?
                         singleLine.posts.map((singleBlock, j) => (
-                            <div key={j}>
-                                <h1>{singleBlock.head}</h1>
-                                <p>{singleBlock.content}</p>
-                            </div>
+                            <Post key={j} data={singleBlock} />
                         ))
                         :
-                        <div>
-                            <h1>{singleLine.head}</h1>
-                            <p>{singleLine.content}</p>
-                        </div>
+                        <Post key={i} data={singleLine} />
                     }
 
                 </div>
@@ -75,3 +75,10 @@ export default () => {
         </div>
     );
 };
+
+let Post = (props) => (
+    <div>
+        <h1>{props.data.head} {props.data.headSub ? <span>{props.data.headSub}</span> : ""}</h1>
+        <p>{props.data.content}</p>
+    </div>
+);
