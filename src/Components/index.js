045d10a756e64fbc62421c38ec9b2946bf72ba93
root <root@DESKTOP-2ABR3V5.localdomain>
2018-02-01 12:56:21 +0530

    Updates

--- src/Components/index.js
+++
@@ -1,7 +0,0 @@
-import MenuList from "./Menu";
-import Board from "./Board";
-import Coordinators from "./Coordinators";
-import Timeline from "./Timeline";
-import Notification from "./Notification";
-
-export { MenuList, Board, Coordinators, Timeline, Notification };
--- src/Data/index.js
+++
@@ -1,7 +0,0 @@
-import BoardData from "./Board.json";
-import MenuData from "./Menu.json";
-import TimelineData from "./Timeline.json";
-import CoordinatorsData from "./Coordinators.json";
-import Notification from "./Notification.json";
-
-export { BoardData, MenuData, TimelineData, CoordinatorsData, Notification };
